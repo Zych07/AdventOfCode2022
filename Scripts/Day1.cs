a545e4687e47a3f798b4577b58ca4c3dbcf57527
Zych07 <39556481+Zych07@users.noreply.github.com>
2022-11-30 08:32:03 +0100

    Initial commit

---
+++ Scripts/Day1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace AdventOfCode2022
+{
+    public class Day1 : Day
+    {
+        public long Part1(string[] lines)
+        {
+            return -1;
+        }
+
+        public long Part2(string[] lines)
+        {
+            return -1;
+        }
+    }
+}
