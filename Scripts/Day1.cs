0623a7aa6a138c7b3d283bcdb49db71acfa4343d
Robert Zych <>
2022-12-01 06:39:16 +0100

    Day1

--- Scripts/Day1.cs
+++ Scripts/Day1.cs
@@ -4,14 +4,40 @@ namespace AdventOfCode2022
 {
     public class Day1 : Day
     {
+        List<long> maxCallories = new List<long>();
+
         public long Part1(string[] lines)
         {
-            return -1;
+            CalculateCallories(lines);
+
+            return maxCallories.Max();
         }
 
         public long Part2(string[] lines)
         {
-            return -1;
+            CalculateCallories(lines);
+
+            long sumOfTopThree = maxCallories.OrderByDescending(x => x).Take(3).Sum();
+
+            return sumOfTopThree;
+        }
+
+        private void CalculateCallories(string[] lines)
+        {
+            maxCallories.Clear();
+            
+            long sumOfcallories = 0;
+
+            foreach (var line in lines)
+            {
+                if (line == "")
+                {
+                    maxCallories.Add(sumOfcallories);
+                    sumOfcallories = 0;
+                }
+                else
+                    sumOfcallories += long.Parse(line);
+            }
         }
     }
 }
