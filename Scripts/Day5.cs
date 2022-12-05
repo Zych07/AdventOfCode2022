4149e7462387dfe05c340b833dfe558268907ee1
Robert Zych <>
2022-12-05 07:07:04 +0100

    Day5

---
+++ Scripts/Day5.cs
@@ -0,0 +1,98 @@
+using System;
+namespace AdventOfCode2022
+{
+    public class Day5 : Day
+    {
+        const int STEP = 4;
+        const int NUM_OF_STACK = 9;
+
+        LinkedList<char>[] stacks = new LinkedList<char>[NUM_OF_STACK];
+
+        public long Part1(string[] lines)
+        {
+            InitInput(lines);
+
+            PrintResult();
+            return 0;
+        }
+        public long Part2(string[] lines)
+        {
+            InitInput(lines, true);
+
+            PrintResult();
+            return 0;
+        }
+
+
+        private void InitInput(string[] lines, bool crateMover9001 = false)
+        {
+            for (int i = 0; i < NUM_OF_STACK; i++)
+                stacks[i] = new LinkedList<char>();
+
+
+            bool startMoveInput = false;
+            foreach (var line in lines)
+            {
+                if (line.Length <= 1)
+                {
+                    startMoveInput = true;
+                    continue;
+                }
+
+                if (!startMoveInput)
+                    AddCreateToStack(line);
+                else
+                    MoveCreatesFromToStack(crateMover9001, line);
+            }
+        }
+
+        private void AddCreateToStack(string line)
+        {
+            for (int i = 0; i < NUM_OF_STACK; i++)
+            {
+                char create = line.Substring(1 + (STEP * i), 1)[0];
+
+                if (char.IsLetter(create))
+                    stacks[i].AddFirst(create);
+            }
+        }
+        private void MoveCreatesFromToStack(bool crateMover9001, string line)
+        {
+            var moveCreateInfo = line.Split(' ');
+            int howManyCreates = int.Parse(moveCreateInfo[1]);
+            int fromStack = int.Parse(moveCreateInfo[3]) - 1;
+            int toStack = int.Parse(moveCreateInfo[5]) - 1;
+
+            string tmp = "";
+
+            for (int i = 0; i < howManyCreates; i++)
+            {
+                var create = stacks[fromStack].Last;
+                stacks[fromStack].RemoveLast();
+
+                if (!crateMover9001)
+                {
+                    stacks[toStack].AddLast(create.Value);
+                    continue;
+                }
+
+                tmp = create.Value + tmp;
+                if (i == howManyCreates - 1)
+                    foreach (var t in tmp)
+                        stacks[toStack].AddLast(t);
+            }
+        }
+        private void PrintResult()
+        {
+            string result = "";
+            for (int i = 0; i < NUM_OF_STACK; i++)
+            {
+                if (stacks[i].Count == 0)
+                    continue;
+
+                result += stacks[i].Last();
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
